c423b16703e0f7e5da51b66d74394389048adab1
Ivy Jha <ivy@ivyj.me>
2015-05-28 01:53:23 -0700

    Rudimentary word highlighter

--- src/background.js
+++ src/background.js
@@ -1,12 +1,12 @@
+chrome.browserAction.setBadgeBackgroundColor({
+    color: [0, 128, 196, 255]
+});
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.type == 'setBadgeText') {
         chrome.browserAction.setBadgeText({
-            text: sender.tab.id + '',
+            text: request.text,
             tabId: sender.tab.id
         });
     }
-});
-
-chrome.browserAction.setBadgeBackgroundColor({
-    color: [0, 128, 196, 255]
 });
--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,49 @@
-chrome.runtime.sendMessage({
-    type: 'setBadgeText',
-    text: 5 + ''
-});
+// Get translation from the translation server, and call the callback with error if there was any and the translated word as the parameter.
+function getTranslation(word, callback) {
+    // var request = new XMLHttpRequest();
+    // request.onload = function() {
+    //     if (request.responseText.length == 0) {
+    //         callback(true, null);
+    //     }
+    //     else {
+    //         var word = JSON.parse(request.responseText[0].text);
+    //         callback(false, word);
+    //     }
+    // };
+    // request.open('GET', 'http://deu.hablaa.com/hs/translation/' + word + '/eng-deu/', true);
+    // request.send();
+    if (Math.random() < 0.5) {
+        callback(true, null);
+    }
+    else {
+        callback(false, 'translation');
+    }
+}
+
+var words = document.body.innerHTML.split(' ');
+
+var translations = {};
+var numWords = 0;
+(
+    function getNextWord() {
+        if (numWords == 25) {
+            document.body.innerHTML = words.join(' ');
+        }
+        else {
+            var index = Math.floor(Math.random() * words.length);
+
+            if (/^[a-zA-Z]+$/.test(words[index])) {
+                getTranslation(words[index], function(error, translation) {
+                    if (!error) {
+                        console.log(translation);
+                        translations[words[index]] = translation;
+                        words[index] = '<span class="quiz">' + words[index] + '</span>';
+                        numWords++;
+                    }
+                });
+            }
+
+            getNextWord();
+        }
+    }
+)();
