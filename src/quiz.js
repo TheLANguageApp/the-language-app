bf21230f4c5b175a6bd043de2af4baf5d5ece581
Ivy Jha <ivy@ivyj.me>
2015-06-09 00:05:54 -0700

    Restyle quiz display (Fixes #2)

--- src/quiz.js
+++ src/quiz.js
@@ -189,9 +189,8 @@ function startQuiz(practiceWord) {
 
         // <br /> is equivalent to new line
         var prompt = document.createElement('p'); // prompt is a paragraph (<p> is paragraph tag)
-        prompt.innerHTML = 'Please select the correct German <br /> Translation of "' + practiceWord
-              + '" <br /> <br /> Bitte wählen Sie die richtige deutsche'
-              + '<br /> Übersetzung von "' + practiceWord + '"';
+        prompt.innerHTML = 'Was ist der richtige Deutsche übersetzung von "' + practiceWord + '"?' +
+          '<br>What is the correct German translation of "' + practiceWord + '"?';
         element.appendChild(prompt);
 
         var feedback = document.createElement('p'); // dynamic feedback (correct/incorrect)
@@ -204,26 +203,25 @@ function startQuiz(practiceWord) {
             // correct choice for quiz
             // elt is prompt
             if (word == practiceWord) {
-                button.onclick = function(elt, elt2, btn) {
+                button.onclick = function(elt, elt2, btn, btnContainer) {
                     return function() {
-                        elt2.innerHTML = "";
+                        buttonContainer.style.display = 'none';
+                        elt2.style.display = 'none';
                         elt.innerHTML = 'Congratulations, you got it right!';
-                        button.disabled = true;
                         chrome.runtime.sendMessage({ type: 'incrementProgress' });
                         remainingQuizzes--;
                         chrome.runtime.sendMessage({ type: 'setBadgeText', text: remainingQuizzes + '' });
                     }
-                }(feedback, prompt, button); // calls itself immediately
+                }(feedback, prompt, button, buttonContainer); // calls itself immediately
             }
             // incorrect choice in button
             // elt is feedback, elt2 is prompt
             else {
                 button.onclick = function(elt, elt2, wrd, btn) {
                     return function() {
-                        elt2.innerHTML = "Sorry, that was not correct <br> Leider war das nicht rightig";
-                        elt.innerHTML = "But now you know that " + btn.innerHTML + " means " + wrd
-                            + "<br> Try again!<br><br> Aber jetzt wissen Sie, dass " + btn.innerHTML
-                            + "<br>" + wrd + " bedeutet! <br> Versuchen Sie es erneut!";
+                        elt2.innerHTML = "Leider war das nicht rightig.<br>Sorry, that was not correct.";
+                        elt.innerHTML = "Aber jetzt wissen sie, dass " + btn.innerHTML + ' ' + wrd + ' bedeutet!' +
+                          '<br>But now you know that ' + btn.innerHTML + ' means ' + wrd + "! Try again!";
                         btn.disabled = true;
                     }
                 }(feedback, prompt, word, button); // calls itself immediately
@@ -233,11 +231,14 @@ function startQuiz(practiceWord) {
         for (var i = 0; i <= buttonContainer.children.length; i++) { // randomize the choices
             buttonContainer.appendChild(buttonContainer.children[Math.random() * i | 0]);
         }
+        buttonContainer.style.marginTop = '10px';
+        buttonContainer.style.marginBottom = '10px';
 
         element.appendChild(feedback); // adds the feedback prompt space to the quiz window
 
         var close = document.createElement('button'); // create 'Close quiz' button
         close.innerHTML = 'Close quiz';
+        close.className = 'close';
         // onclick it close the quizWindow
         close.onclick = function() {
             document.body.removeChild(element);
